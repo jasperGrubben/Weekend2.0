311f9ac4f4c473e1d2ae22839be8c8f9c49a33c3
Jaspercgi <112546481+jasperGrubben@users.noreply.github.com>
2023-09-11 14:52:24 +0200

    backup basis

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Weekend.leerling;
 
 namespace Weekend
 {
@@ -18,59 +19,49 @@ namespace Weekend
             InitializeComponent();
         }
 
-        private void btnRegistatie_Click(object sender, EventArgs e)
+        private void btnLeerling_Click(object sender, EventArgs e)
         {
-
+            var temp = new leerling.leerling();
+            temp.Show();
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private Button button1;
+
+        private void InitializeComponent()
         {
-        }
-private void InitializeComponent()
-        {
-            this.btnLeerling = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnLeerling
+            // button1
             // 
-            this.btnLeerling.Location = new System.Drawing.Point(1108, 42);
-            this.btnLeerling.Name = "btnLeerling";
-            this.btnLeerling.Size = new System.Drawing.Size(163, 65);
-            this.btnLeerling.TabIndex = 0;
-            this.btnLeerling.Text = "leerling";
-            this.btnLeerling.UseVisualStyleBackColor = true;
-            this.btnLeerling.Click += new System.EventHandler(this.btnLeerling_Click);
+            this.button1.Location = new System.Drawing.Point(352, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1543, 566);
-            this.Controls.Add(this.btnLeerling);
+            this.ClientSize = new System.Drawing.Size(1282, 618);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
-        private Button btnLeerling;
-
-        private void btnLeerling_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "C# Files (*.cs)|*.cs";
-            openFileDialog1.Title = "Open a C# File";
 
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                string filename = openFileDialog1.FileName;
+        }
 
-                using (StreamReader sr = new StreamReader(filename))
-                {
-                    string s = "";
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(s);
-                    }
-                }
-            }
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            var temp = new leerling.leerling();
+            temp.Show();
+            this.Close();
         }
     }
 }
