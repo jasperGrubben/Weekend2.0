867ef313bc2c79f35afd66713c6ca022bc0d9c9d
Jaspercgi <112546481+jasperGrubben@users.noreply.github.com>
2023-09-11 14:58:54 +0200

    menu aangemaakt, beetje layout van de leerlingen app

--- App/Weekend/Weekend/leerling/leerling.Designer.cs
+++ App/Weekend/Weekend/leerling/leerling.Designer.cs
@@ -28,19 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.leerlingenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highScoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblWelkom = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.leerlingenToolStripMenuItem,
+            this.scoresToolStripMenuItem,
+            this.highScoresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1592, 48);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // leerlingenToolStripMenuItem
+            // 
+            this.leerlingenToolStripMenuItem.Name = "leerlingenToolStripMenuItem";
+            this.leerlingenToolStripMenuItem.Size = new System.Drawing.Size(141, 44);
+            this.leerlingenToolStripMenuItem.Text = "leerlingen";
+            // 
+            // scoresToolStripMenuItem
+            // 
+            this.scoresToolStripMenuItem.Name = "scoresToolStripMenuItem";
+            this.scoresToolStripMenuItem.Size = new System.Drawing.Size(100, 44);
+            this.scoresToolStripMenuItem.Text = "scores";
+            // 
+            // highScoresToolStripMenuItem
+            // 
+            this.highScoresToolStripMenuItem.Name = "highScoresToolStripMenuItem";
+            this.highScoresToolStripMenuItem.Size = new System.Drawing.Size(161, 44);
+            this.highScoresToolStripMenuItem.Text = "High Scores";
+            // 
+            // lblWelkom
+            // 
+            this.lblWelkom.AutoSize = true;
+            this.lblWelkom.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelkom.Location = new System.Drawing.Point(66, 150);
+            this.lblWelkom.Name = "lblWelkom";
+            this.lblWelkom.Size = new System.Drawing.Size(210, 55);
+            this.lblWelkom.TabIndex = 2;
+            this.lblWelkom.Text = "Welkom,";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 55);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sussy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(425, 55);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Het is vandaag de:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(522, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 55);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Datum";
             // 
             // leerling
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1592, 761);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblWelkom);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "leerling";
             this.Text = "leerling";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem leerlingenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highScoresToolStripMenuItem;
+        private System.Windows.Forms.Label lblWelkom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
