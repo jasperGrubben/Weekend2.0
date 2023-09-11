94971da6b3776a80ac9b6ca79deae17b964b0267
Jaspercgi <112546481+jasperGrubben@users.noreply.github.com>
2023-09-11 11:04:21 +0200

    iets teveel geignoord

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -27,27 +27,33 @@ namespace Weekend
         }
 private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLeerling = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnLeerling
             // 
-            this.button1.Location = new System.Drawing.Point(1108, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 65);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLeerling.Location = new System.Drawing.Point(1108, 42);
+            this.btnLeerling.Name = "btnLeerling";
+            this.btnLeerling.Size = new System.Drawing.Size(163, 65);
+            this.btnLeerling.TabIndex = 0;
+            this.btnLeerling.Text = "leerling";
+            this.btnLeerling.UseVisualStyleBackColor = true;
+            this.btnLeerling.Click += new System.EventHandler(this.btnLeerling_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1543, 566);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLeerling);
             this.Name = "Form1";
             this.ResumeLayout(false);
 
         }
 
-        private Button button1;
+        private Button btnLeerling;
+
+        private void btnLeerling_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
