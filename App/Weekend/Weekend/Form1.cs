fe8ab211786201fa064e3db7eea89244701a7120
Jaspercgi <112546481+jasperGrubben@users.noreply.github.com>
2023-09-11 11:00:34 +0200

    test

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -27,14 +27,27 @@ namespace Weekend
         }
 private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(943, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1543, 566);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.ResumeLayout(false);
 
         }
+
+        private Button button1;
     }
 }
