4d0ce9221baab5dad14240634af5a818a45affae
TomBackes <112612019+TomBackes@users.noreply.github.com>
2023-09-15 12:00:28 +0200

    push 3

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -29,19 +29,23 @@ namespace Weekend
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.btnInloggenLeerling = new System.Windows.Forms.Button();
             this.btnRegisteren = new System.Windows.Forms.Button();
             this.pnlLogInLeerling = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.pnlLogInLeerling.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 377);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(337, 421);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(359, 108);
+            this.button1.Size = new System.Drawing.Size(307, 99);
             this.button1.TabIndex = 0;
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,8 +73,10 @@ namespace Weekend
             // 
             // pnlLogInLeerling
             // 
+            this.pnlLogInLeerling.Controls.Add(this.label6);
+            this.pnlLogInLeerling.Controls.Add(this.btnCancel);
             this.pnlLogInLeerling.Controls.Add(this.button1);
-            this.pnlLogInLeerling.Location = new System.Drawing.Point(563, 67);
+            this.pnlLogInLeerling.Location = new System.Drawing.Point(689, 12);
             this.pnlLogInLeerling.Name = "pnlLogInLeerling";
             this.pnlLogInLeerling.Size = new System.Drawing.Size(668, 546);
             this.pnlLogInLeerling.TabIndex = 3;
@@ -85,6 +91,26 @@ namespace Weekend
             this.label5.TabIndex = 4;
             this.label5.Text = "Welkom";
             // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(24, 421);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(307, 99);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Annuleer";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(77, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Email";
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1924, 1049);
@@ -92,9 +118,11 @@ namespace Weekend
             this.Controls.Add(this.btnRegisteren);
             this.Controls.Add(this.pnlLogInLeerling);
             this.Controls.Add(this.btnInloggenLeerling);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.pnlLogInLeerling.ResumeLayout(false);
+            this.pnlLogInLeerling.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,5 +169,7 @@ namespace Weekend
         }
 
         private Label label5;
+        private Button btnCancel;
+        private Label label6;
     }
 }
