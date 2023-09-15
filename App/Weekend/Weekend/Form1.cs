184c58574333b5d16d2b7fa5542d7e953e7d5c5f
TomBackes <112612019+TomBackes@users.noreply.github.com>
2023-09-15 12:10:46 +0200

    push 4

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -34,16 +34,18 @@ namespace Weekend
             this.btnInloggenLeerling = new System.Windows.Forms.Button();
             this.btnRegisteren = new System.Windows.Forms.Button();
             this.pnlLogInLeerling = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btnCancel = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.pnlLogInLeerling.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(337, 421);
+            this.button1.Location = new System.Drawing.Point(337, 223);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(307, 99);
             this.button1.TabIndex = 0;
@@ -53,6 +55,7 @@ namespace Weekend
             // 
             // btnInloggenLeerling
             // 
+            this.btnInloggenLeerling.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInloggenLeerling.Location = new System.Drawing.Point(40, 175);
             this.btnInloggenLeerling.Name = "btnInloggenLeerling";
             this.btnInloggenLeerling.Size = new System.Drawing.Size(329, 113);
@@ -63,6 +66,7 @@ namespace Weekend
             // 
             // btnRegisteren
             // 
+            this.btnRegisteren.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegisteren.Location = new System.Drawing.Point(40, 310);
             this.btnRegisteren.Name = "btnRegisteren";
             this.btnRegisteren.Size = new System.Drawing.Size(329, 113);
@@ -73,43 +77,66 @@ namespace Weekend
             // 
             // pnlLogInLeerling
             // 
+            this.pnlLogInLeerling.Controls.Add(this.label8);
+            this.pnlLogInLeerling.Controls.Add(this.label7);
             this.pnlLogInLeerling.Controls.Add(this.label6);
             this.pnlLogInLeerling.Controls.Add(this.btnCancel);
             this.pnlLogInLeerling.Controls.Add(this.button1);
-            this.pnlLogInLeerling.Location = new System.Drawing.Point(689, 12);
+            this.pnlLogInLeerling.Location = new System.Drawing.Point(49, 429);
             this.pnlLogInLeerling.Name = "pnlLogInLeerling";
-            this.pnlLogInLeerling.Size = new System.Drawing.Size(668, 546);
+            this.pnlLogInLeerling.Size = new System.Drawing.Size(668, 347);
             this.pnlLogInLeerling.TabIndex = 3;
             // 
-            // label5
+            // label8
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Rockwell Nova Cond", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(127, 38);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(156, 54);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Welkom";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Rockwell Nova Cond", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(133, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(408, 80);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Leerling Log In";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(77, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Wachtwoord:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(107, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Email:";
             // 
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(24, 421);
+            this.btnCancel.Location = new System.Drawing.Point(24, 224);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(307, 99);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Annuleer";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // label5
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(77, 64);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 25);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Email";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell Nova Cond", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(127, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(234, 80);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Welkom";
             // 
             // Form1
             // 
@@ -120,6 +147,7 @@ namespace Weekend
             this.Controls.Add(this.btnInloggenLeerling);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.Text = "RekenApp - Home";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.pnlLogInLeerling.ResumeLayout(false);
             this.pnlLogInLeerling.PerformLayout();
@@ -171,5 +199,12 @@ namespace Weekend
         private Label label5;
         private Button btnCancel;
         private Label label6;
+        private Label label7;
+        private Label label8;
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
