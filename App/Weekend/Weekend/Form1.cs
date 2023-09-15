9bb5706431e12e523e983a159ed1d7aa6125d65e
TomBackes <112612019+TomBackes@users.noreply.github.com>
2023-09-15 11:48:06 +0200

    login shit geupdate

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -32,57 +32,71 @@ namespace Weekend
             this.button1 = new System.Windows.Forms.Button();
             this.btnInloggenLeerling = new System.Windows.Forms.Button();
             this.btnRegisteren = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.pnlLogInLeerling = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pnlLogInLeerling.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(144, 299);
+            this.button1.Location = new System.Drawing.Point(153, 377);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(359, 108);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnInloggenLeerling
             // 
-            this.btnInloggenLeerling.Location = new System.Drawing.Point(59, 62);
+            this.btnInloggenLeerling.Location = new System.Drawing.Point(40, 175);
             this.btnInloggenLeerling.Name = "btnInloggenLeerling";
-            this.btnInloggenLeerling.Size = new System.Drawing.Size(149, 84);
+            this.btnInloggenLeerling.Size = new System.Drawing.Size(329, 113);
             this.btnInloggenLeerling.TabIndex = 1;
             this.btnInloggenLeerling.Text = "Log in \r\nLeerlingen";
             this.btnInloggenLeerling.UseVisualStyleBackColor = true;
+            this.btnInloggenLeerling.Click += new System.EventHandler(this.btnInloggenLeerling_Click);
             // 
             // btnRegisteren
             // 
-            this.btnRegisteren.Location = new System.Drawing.Point(1662, 406);
+            this.btnRegisteren.Location = new System.Drawing.Point(40, 310);
             this.btnRegisteren.Name = "btnRegisteren";
-            this.btnRegisteren.Size = new System.Drawing.Size(304, 109);
+            this.btnRegisteren.Size = new System.Drawing.Size(329, 113);
             this.btnRegisteren.TabIndex = 2;
             this.btnRegisteren.Text = "Registeren";
             this.btnRegisteren.UseVisualStyleBackColor = true;
             this.btnRegisteren.Click += new System.EventHandler(this.btnRegisteren_Click);
             // 
-            // panel1
+            // pnlLogInLeerling
             // 
-            this.panel1.Controls.Add(this.btnInloggenLeerling);
-            this.panel1.Location = new System.Drawing.Point(931, 176);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1035, 668);
-            this.panel1.TabIndex = 3;
+            this.pnlLogInLeerling.Controls.Add(this.button1);
+            this.pnlLogInLeerling.Location = new System.Drawing.Point(563, 67);
+            this.pnlLogInLeerling.Name = "pnlLogInLeerling";
+            this.pnlLogInLeerling.Size = new System.Drawing.Size(668, 546);
+            this.pnlLogInLeerling.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell Nova Cond", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(127, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 54);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Welkom";
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1924, 1049);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnRegisteren);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pnlLogInLeerling);
+            this.Controls.Add(this.btnInloggenLeerling);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
+            this.pnlLogInLeerling.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,14 +106,14 @@ namespace Weekend
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {            
+        {
             var temp = new leerling.leerling();
             temp.Show();
         }
 
         private Button btnInloggenLeerling;
         private Button btnRegisteren;
-        private Panel panel1;
+        private Panel pnlLogInLeerling;
 
         private void btnRegisteren_Click(object sender, EventArgs e)
         {
@@ -120,5 +134,12 @@ namespace Weekend
         private Button btnTerug;
         private Label label4;
         private TextBox txtNaam;
+
+        private void btnInloggenLeerling_Click(object sender, EventArgs e)
+        {
+            pnlLogInLeerling.Show();
+        }
+
+        private Label label5;
     }
 }
