246d5f94c28b37d5700d7a97afabfbbc37befb12
Sjoerd <sjoerd.van.veen@student.gildeopleidingen.nl>
2023-09-15 11:44:47 +0200

    leerling en klas

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -28,6 +28,7 @@ namespace Weekend
 private void InitializeComponent()
         {
             this.btnLeerling = new System.Windows.Forms.Button();
+            this.btnDocent = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLeerling
@@ -40,9 +41,20 @@ private void InitializeComponent()
             this.btnLeerling.UseVisualStyleBackColor = true;
             this.btnLeerling.Click += new System.EventHandler(this.btnLeerling_Click);
             // 
+            // btnDocent
+            // 
+            this.btnDocent.Location = new System.Drawing.Point(488, 158);
+            this.btnDocent.Name = "btnDocent";
+            this.btnDocent.Size = new System.Drawing.Size(137, 66);
+            this.btnDocent.TabIndex = 1;
+            this.btnDocent.Text = "docent";
+            this.btnDocent.UseVisualStyleBackColor = true;
+            this.btnDocent.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1543, 566);
+            this.Controls.Add(this.btnDocent);
             this.Controls.Add(this.btnLeerling);
             this.Name = "Form1";
             this.ResumeLayout(false);
@@ -53,10 +65,15 @@ private void InitializeComponent()
 
         private void btnLeerling_Click(object sender, EventArgs e)
         {
-            Process notePad = new Process();
-            notePad.StartInfo.FileName = "notepad.exe";
-            notePad.StartInfo.Arguments = "ProcessStart.cs"; // if you need some
-            notePad.Start();
+            
+        }
+
+        private Button btnDocent;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var docent = new docent.Form1();
+            docent.Show();
         }
     }
 }
--- App/Weekend/Weekend/docent/docent.Designer.cs
+++ App/Weekend/Weekend/docent/docent.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Weekend.docent
 {
-    partial class Docent.cs
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -33,16 +33,22 @@
             this.lblALInummer = new System.Windows.Forms.Label();
             this.lblALI = new System.Windows.Forms.Label();
             this.lblAGO = new System.Windows.Forms.Label();
-            this.pnlLeerlingDocentPagina = new System.Windows.Forms.Panel();
-            this.lblLaatsteOpdracht = new System.Windows.Forms.Label();
-            this.lblLaatstIngelogd = new System.Windows.Forms.Label();
-            this.lblLeerling = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.leerlingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.klassenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlLeerling = new System.Windows.Forms.Panel();
+            this.lblLaatstIngelogd = new System.Windows.Forms.Label();
+            this.lblLeerling = new System.Windows.Forms.Label();
+            this.lblLaatsteOpdracht = new System.Windows.Forms.Label();
+            this.pnlKlassen = new System.Windows.Forms.Panel();
+            this.lblLeerling2 = new System.Windows.Forms.Label();
+            this.lblKlas = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.pnlLeerlingDocentPagina.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.pnlLeerling.SuspendLayout();
+            this.pnlKlassen.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -92,58 +98,13 @@
             this.lblAGO.TabIndex = 0;
             this.lblAGO.Text = "Aantal gemaakte opdrachten:";
             // 
-            // pnlLeerlingDocentPagina
-            // 
-            this.pnlLeerlingDocentPagina.Controls.Add(this.lblLaatsteOpdracht);
-            this.pnlLeerlingDocentPagina.Controls.Add(this.lblLaatstIngelogd);
-            this.pnlLeerlingDocentPagina.Controls.Add(this.lblLeerling);
-            this.pnlLeerlingDocentPagina.Controls.Add(this.textBox1);
-            this.pnlLeerlingDocentPagina.Location = new System.Drawing.Point(12, 39);
-            this.pnlLeerlingDocentPagina.Name = "pnlLeerlingDocentPagina";
-            this.pnlLeerlingDocentPagina.Size = new System.Drawing.Size(957, 425);
-            this.pnlLeerlingDocentPagina.TabIndex = 3;
-            this.pnlLeerlingDocentPagina.Visible = false;
-            // 
-            // lblLaatsteOpdracht
-            // 
-            this.lblLaatsteOpdracht.AutoSize = true;
-            this.lblLaatsteOpdracht.Location = new System.Drawing.Point(682, 57);
-            this.lblLaatsteOpdracht.Name = "lblLaatsteOpdracht";
-            this.lblLaatsteOpdracht.Size = new System.Drawing.Size(103, 16);
-            this.lblLaatsteOpdracht.TabIndex = 3;
-            this.lblLaatsteOpdracht.Text = "laatste opdracht";
-            // 
-            // lblLaatstIngelogd
-            // 
-            this.lblLaatstIngelogd.AutoSize = true;
-            this.lblLaatstIngelogd.Location = new System.Drawing.Point(430, 57);
-            this.lblLaatstIngelogd.Name = "lblLaatstIngelogd";
-            this.lblLaatstIngelogd.Size = new System.Drawing.Size(95, 16);
-            this.lblLaatstIngelogd.TabIndex = 2;
-            this.lblLaatstIngelogd.Text = "laatst ingelogd";
-            // 
-            // lblLeerling
-            // 
-            this.lblLeerling.AutoSize = true;
-            this.lblLeerling.Location = new System.Drawing.Point(165, 57);
-            this.lblLeerling.Name = "lblLeerling";
-            this.lblLeerling.Size = new System.Drawing.Size(51, 16);
-            this.lblLeerling.TabIndex = 1;
-            this.lblLeerling.Text = "leerling";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(88, 35);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(771, 350);
-            this.textBox1.TabIndex = 0;
-            // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.leerlingToolStripMenuItem});
+            this.leerlingToolStripMenuItem,
+            this.klassenToolStripMenuItem,
+            this.gamesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(989, 28);
@@ -157,12 +118,103 @@
             this.leerlingToolStripMenuItem.Text = "leerling";
             this.leerlingToolStripMenuItem.Click += new System.EventHandler(this.leerlingToolStripMenuItem_Click);
             // 
+            // klassenToolStripMenuItem
+            // 
+            this.klassenToolStripMenuItem.Name = "klassenToolStripMenuItem";
+            this.klassenToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.klassenToolStripMenuItem.Text = "klassen";
+            this.klassenToolStripMenuItem.Click += new System.EventHandler(this.klassenToolStripMenuItem_Click);
+            // 
+            // gamesToolStripMenuItem
+            // 
+            this.gamesToolStripMenuItem.Name = "gamesToolStripMenuItem";
+            this.gamesToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.gamesToolStripMenuItem.Text = "games";
+            this.gamesToolStripMenuItem.Click += new System.EventHandler(this.gamesToolStripMenuItem_Click);
+            // 
+            // pnlLeerling
+            // 
+            this.pnlLeerling.Controls.Add(this.lblLaatsteOpdracht);
+            this.pnlLeerling.Controls.Add(this.lblLeerling);
+            this.pnlLeerling.Controls.Add(this.lblLaatstIngelogd);
+            this.pnlLeerling.Location = new System.Drawing.Point(46, 94);
+            this.pnlLeerling.Name = "pnlLeerling";
+            this.pnlLeerling.Size = new System.Drawing.Size(829, 347);
+            this.pnlLeerling.TabIndex = 5;
+            this.pnlLeerling.Visible = false;
+            // 
+            // lblLaatstIngelogd
+            // 
+            this.lblLaatstIngelogd.AutoSize = true;
+            this.lblLaatstIngelogd.Location = new System.Drawing.Point(339, 18);
+            this.lblLaatstIngelogd.Name = "lblLaatstIngelogd";
+            this.lblLaatstIngelogd.Size = new System.Drawing.Size(95, 16);
+            this.lblLaatstIngelogd.TabIndex = 2;
+            this.lblLaatstIngelogd.Text = "laatst ingelogd";
+            // 
+            // lblLeerling
+            // 
+            this.lblLeerling.AutoSize = true;
+            this.lblLeerling.Location = new System.Drawing.Point(45, 18);
+            this.lblLeerling.Name = "lblLeerling";
+            this.lblLeerling.Size = new System.Drawing.Size(51, 16);
+            this.lblLeerling.TabIndex = 1;
+            this.lblLeerling.Text = "leerling";
+            // 
+            // lblLaatsteOpdracht
+            // 
+            this.lblLaatsteOpdracht.AutoSize = true;
+            this.lblLaatsteOpdracht.Location = new System.Drawing.Point(640, 18);
+            this.lblLaatsteOpdracht.Name = "lblLaatsteOpdracht";
+            this.lblLaatsteOpdracht.Size = new System.Drawing.Size(103, 16);
+            this.lblLaatsteOpdracht.TabIndex = 3;
+            this.lblLaatsteOpdracht.Text = "laatste opdracht";
+            // 
+            // pnlKlassen
+            // 
+            this.pnlKlassen.Controls.Add(this.lblLeerling2);
+            this.pnlKlassen.Controls.Add(this.lblKlas);
+            this.pnlKlassen.Controls.Add(this.label3);
+            this.pnlKlassen.Location = new System.Drawing.Point(87, 31);
+            this.pnlKlassen.Name = "pnlKlassen";
+            this.pnlKlassen.Size = new System.Drawing.Size(890, 413);
+            this.pnlKlassen.TabIndex = 4;
+            this.pnlKlassen.Visible = false;
+            // 
+            // lblLeerling2
+            // 
+            this.lblLeerling2.AutoSize = true;
+            this.lblLeerling2.Location = new System.Drawing.Point(52, 28);
+            this.lblLeerling2.Name = "lblLeerling2";
+            this.lblLeerling2.Size = new System.Drawing.Size(51, 16);
+            this.lblLeerling2.TabIndex = 5;
+            this.lblLeerling2.Text = "leerling";
+            // 
+            // lblKlas
+            // 
+            this.lblKlas.AutoSize = true;
+            this.lblKlas.Location = new System.Drawing.Point(345, 28);
+            this.lblKlas.Name = "lblKlas";
+            this.lblKlas.Size = new System.Drawing.Size(32, 16);
+            this.lblKlas.TabIndex = 6;
+            this.lblKlas.Text = "klas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "......fjda";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(989, 508);
-            this.Controls.Add(this.pnlLeerlingDocentPagina);
+            this.Controls.Add(this.pnlKlassen);
+            this.Controls.Add(this.pnlLeerling);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -170,10 +222,12 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.pnlLeerlingDocentPagina.ResumeLayout(false);
-            this.pnlLeerlingDocentPagina.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.pnlLeerling.ResumeLayout(false);
+            this.pnlLeerling.PerformLayout();
+            this.pnlKlassen.ResumeLayout(false);
+            this.pnlKlassen.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,12 +240,17 @@
         private System.Windows.Forms.Label lblAGOnummer;
         private System.Windows.Forms.Label lblALInummer;
         private System.Windows.Forms.Label lblALI;
-        private System.Windows.Forms.Panel pnlLeerlingDocentPagina;
-        private System.Windows.Forms.Label lblLaatsteOpdracht;
-        private System.Windows.Forms.Label lblLaatstIngelogd;
-        private System.Windows.Forms.Label lblLeerling;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem leerlingToolStripMenuItem;
-        }
+        private System.Windows.Forms.ToolStripMenuItem klassenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gamesToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlLeerling;
+        private System.Windows.Forms.Label lblLaatsteOpdracht;
+        private System.Windows.Forms.Label lblLeerling;
+        private System.Windows.Forms.Label lblLaatstIngelogd;
+        private System.Windows.Forms.Panel pnlKlassen;
+        private System.Windows.Forms.Label lblLeerling2;
+        private System.Windows.Forms.Label lblKlas;
+        private System.Windows.Forms.Label label3;
+    }
     }
