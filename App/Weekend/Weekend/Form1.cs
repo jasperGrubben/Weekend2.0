e6fab61275a0ea1fa0805bd691abf028d009a0fc
TomBackes <112612019+TomBackes@users.noreply.github.com>
2023-09-15 12:43:31 +0200

    leraar log in panel done

--- App/Weekend/Weekend/Form1.cs
+++ App/Weekend/Weekend/Form1.cs
@@ -41,7 +41,16 @@ namespace Weekend
             this.label5 = new System.Windows.Forms.Label();
             this.txtEmailStudent = new System.Windows.Forms.TextBox();
             this.txtWachtwoordStudent = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtWachtwoordLeraar = new System.Windows.Forms.TextBox();
+            this.txtEmailLeraar = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnCancelLeraar = new System.Windows.Forms.Button();
+            this.btnLogInLeraar = new System.Windows.Forms.Button();
             this.pnlLogInLeerling.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -95,11 +104,11 @@ namespace Weekend
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Rockwell Nova Cond", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(133, 13);
+            this.label8.Location = new System.Drawing.Point(141, 13);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(408, 80);
+            this.label8.Size = new System.Drawing.Size(388, 80);
             this.label8.TabIndex = 5;
-            this.label8.Text = "Leerling Log In";
+            this.label8.Text = "Student Log In";
             this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label7
@@ -156,9 +165,88 @@ namespace Weekend
             this.txtWachtwoordStudent.Size = new System.Drawing.Size(272, 22);
             this.txtWachtwoordStudent.TabIndex = 7;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtWachtwoordLeraar);
+            this.panel1.Controls.Add(this.txtEmailLeraar);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.btnCancelLeraar);
+            this.panel1.Controls.Add(this.btnLogInLeraar);
+            this.panel1.Location = new System.Drawing.Point(750, 566);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(668, 347);
+            this.panel1.TabIndex = 5;
+            // 
+            // txtWachtwoordLeraar
+            // 
+            this.txtWachtwoordLeraar.Location = new System.Drawing.Point(213, 161);
+            this.txtWachtwoordLeraar.Name = "txtWachtwoordLeraar";
+            this.txtWachtwoordLeraar.Size = new System.Drawing.Size(272, 22);
+            this.txtWachtwoordLeraar.TabIndex = 7;
+            // 
+            // txtEmailLeraar
+            // 
+            this.txtEmailLeraar.Location = new System.Drawing.Point(213, 118);
+            this.txtEmailLeraar.Name = "txtEmailLeraar";
+            this.txtEmailLeraar.Size = new System.Drawing.Size(272, 22);
+            this.txtEmailLeraar.TabIndex = 6;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Rockwell Nova Cond", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(152, 13);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(363, 80);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Leraar Log In";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(77, 157);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(128, 25);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Wachtwoord:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(107, 114);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(66, 25);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Email:";
+            // 
+            // btnCancelLeraar
+            // 
+            this.btnCancelLeraar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelLeraar.Location = new System.Drawing.Point(24, 224);
+            this.btnCancelLeraar.Name = "btnCancelLeraar";
+            this.btnCancelLeraar.Size = new System.Drawing.Size(307, 99);
+            this.btnCancelLeraar.TabIndex = 1;
+            this.btnCancelLeraar.Text = "Annuleer";
+            this.btnCancelLeraar.UseVisualStyleBackColor = true;
+            // 
+            // btnLogInLeraar
+            // 
+            this.btnLogInLeraar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogInLeraar.Location = new System.Drawing.Point(337, 223);
+            this.btnLogInLeraar.Name = "btnLogInLeraar";
+            this.btnLogInLeraar.Size = new System.Drawing.Size(307, 99);
+            this.btnLogInLeraar.TabIndex = 0;
+            this.btnLogInLeraar.Text = "Log In";
+            this.btnLogInLeraar.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1924, 1049);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnRegisteren);
             this.Controls.Add(this.pnlLogInLeerling);
@@ -169,6 +257,8 @@ namespace Weekend
             this.Load += new System.EventHandler(this.Form1_Load);
             this.pnlLogInLeerling.ResumeLayout(false);
             this.pnlLogInLeerling.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,5 +317,13 @@ namespace Weekend
 
         private TextBox txtEmailStudent;
         private TextBox txtWachtwoordStudent;
+        private Panel panel1;
+        private TextBox txtWachtwoordLeraar;
+        private TextBox txtEmailLeraar;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Button btnCancelLeraar;
+        private Button btnLogInLeraar;
     }
 }
